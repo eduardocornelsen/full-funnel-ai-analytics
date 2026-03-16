1091afedb6dd8a1d7f148897b21d8f1227c0053f
eduardocornelsen <eduardo.cornelsen@gmail.com>
2026-03-16 00:21:44 -0300

    feat: Update data time range to 2024-2026 in the time spine and Streamlit app, and add a project task list.

--- dbt_project/models/marts/metricflow_time_spine.sql
+++ dbt_project/models/marts/metricflow_time_spine.sql
@@ -6,7 +6,7 @@
 
 SELECT CAST(d AS DATE) AS date_day
 FROM generate_series(
-    '2016-01-01'::DATE,
-    '2019-12-31'::DATE,
+    '2024-01-01'::DATE,
+    '2026-12-31'::DATE,
     INTERVAL '1 day'
 ) AS t(d)
